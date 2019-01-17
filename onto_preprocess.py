61c75d9a2e1c066915ea6148f431ce745685349a
dugu9sword <dugu9sword@163.com>
2019-01-18 02:00:15 +0800

    support for extracting ner & tag from ontonotes4

--- onto_preprocess.py
+++ onto_preprocess.py
@@ -17,7 +17,8 @@ def load_sentences(file_path):
     return ret
 
 
-root_path = "/home/zhouyi/hdd/NER/ontonotes-release-4.0/data/files/data/chinese/annotations/"
+# root_path = "/home/zhouyi/hdd/NER/ontonotes-release-4.0/data/files/data/chinese/annotations/"
+root_path = "/mnt/c/Users/zhouyi/Desktop/ontonotes-release-4.0/data/files/data/chinese/annotations/"
 
 files = []
 for path, d, filelist in os.walk(root_path):
@@ -28,8 +29,10 @@ for path, d, filelist in os.walk(root_path):
 tag_files = OrderedDict()
 ner_files = OrderedDict()
 
+print(files)
+
 for file in files:
-    found = re.search(".*/([^/]*)\.([a-z]*)", file)
+    found = re.search(r".*/([^/]*)\.([a-z]*)", file)
     if found:
         if found.group(2) == 'name':
             ner_files[found.group(1)] = file
@@ -45,16 +48,78 @@ print("检查完毕。")
 ner_files = OrderedDict(sorted(ner_files.items(),
                                key=lambda item: item[0]))
 
-total=0
+
+def extract_ner(raw_str, keep_labels=("PERSON", "GPE", "ORG", "LOC")):
+    ret = []
+    found = re.findall(r'(?:<ENAMEX TYPE="[^"]*"[^>]*>[^<]+</ENAMEX>|[^\s]+)', raw_str)
+    for ele in found:
+        entity = re.search(r'<ENAMEX TYPE="([^"]*)"[^>]*>([^<]+)</ENAMEX>', ele)
+        if entity:
+            entities = re.split(r"\s+", entity.group(2))
+            if entity.group(1) not in keep_labels:
+                for ent in entities:
+                    ret.append((ent, "O"))
+            else:
+                if len(entities) == 1:
+                    ret.append((entities[0], 'S-{}'.format(entity.group(1))))
+                else:
+                    ret.append((entities[0], 'B-{}'.format(entity.group(1))))
+                    if len(entities) >= 3:
+                        for i in range(1, len(entities) - 1):
+                            ret.append((entities[i], 'M-{}'.format(entity.group(1))))
+                    ret.append((entities[-1], 'E-{}'.format(entity.group(1))))
+        else:
+            ret.append((ele, "O"))
+    return ret
+
+
+from buff import log, log_config
+
+log_config("train.txt", "cf")
+# log_config("dev.txt", "cf")
+# log_config("test.txt", "cf")
+
+train_num = 0
+dev_num = 0
+test_num = 0
 for idx in ner_files:
-    print(ner_files[idx])
+    # print(ner_files[idx])
     ner_lines = open(ner_files[idx], encoding='utf8').readlines()
-    print(tag_files[idx])
+    # print(tag_files[idx])
     tag_lines = load_sentences(tag_files[idx])
-    print(len(tag_lines))
-    #print(tag_lines)
-    print(len(ner_lines))
+    # print(len(tag_lines))
+    # print(tag_lines)
+    # print(len(ner_lines))
 
-    total+=len(tag_lines)
+    # print(ner_pairs)
+    # if "暨" in line:
+    #     break
 
-print(total)
+    if "chtb" not in idx:
+        train_num += len(tag_lines)
+        for line_id in range(1, len(ner_lines) - 1):
+            tag_line_no = 0
+
+            ner_line = ner_lines[line_id]
+            tag_line = tag_lines[line_id - 1]
+            ner_pairs = extract_ner(ner_line)
+            for ele in ner_pairs:
+                # print(tag_line[tag_line_no])
+                while True:
+                    # print(tag_line[tag_line_no])
+                    found_tag = re.search(r'\(([^\s]+)\s+{}\)'.format(ele[0]), tag_line[tag_line_no])
+                    tag_line_no += 1
+                    if found_tag:
+                        break
+                log("{} {} {}".format(ele[0], ele[1], found_tag.group(1)))
+                # print(ele[0], found_tag.group(1))
+            # exit()
+            log()
+    else:
+        if re.match(".*[13579]$", idx):
+            print(idx)
+            dev_num += len(tag_lines)
+        else:
+            test_num += len(tag_lines)
+
+print(train_num, dev_num, test_num)
--- test.py
+++ test.py
@@ -1,14 +1,47 @@
-import torch
-import torch.nn.functional as F
-from buff import focal_loss
-from seq_encoder import FofeSeqEncoder, RNNSeqEncoder
+# import torch
+# import torch.nn.functional as F
+# from buff import focal_loss
+# from seq_encoder import FofeSeqEncoder, RNNSeqEncoder
+import re
 
-x = torch.randn(1, 20, 100, requires_grad=True)
-# seq_enc = FofeSeqEncoder(0.5)
-seq_enc = RNNSeqEncoder('lstm', 100, 100)
-out = seq_enc(x)
-pred = torch.nn.Linear(100, 1)(out[:, -1, :])
-loss = torch.nn.functional.mse_loss(pred, torch.tensor([[1.]]))
-print(loss)
-loss.backward()
-print(x.grad)
+raw_str = """
+<ENAMEX TYPE="ORG" E_OFF="1">亚太 经济 暨 合作 会议</ENAMEX> 
+"""
+
+def extract_ner(raw_str):
+    ret = []
+    found = re.findall(r'(?:<ENAMEX TYPE="[^"]*">[^<]+</ENAMEX>|[^\s]+)', raw_str)
+    for ele in found:
+        entity = re.search(r'<ENAMEX TYPE="([^"]*)">([^<]+)</ENAMEX>', ele)
+        if entity:
+            entities = re.split(r"\s+", entity.group(2))
+            if len(entities) == 1:
+                ret.append(('S-{}'.format(entity.group(1)), entities[0]))
+            else:
+                ret.append(('B-{}'.format(entity.group(1)), entities[0]))
+                if len(entities) >= 3:
+                    for i in range(1, len(entities) - 1):
+                        ret.append(('M-{}'.format(entity.group(1)), entities[i]))
+                ret.append(('E-{}'.format(entity.group(1)), entities[-1]))
+        else:
+            ret.append(("O", ele))
+    return ret
+print(ret)
+# print(found)
+# print(found.group(0))
+#
+# print(found.group(1))
+#
+# print(found.group(2))
+
+# tags = []
+# ptr = 0
+# while True:
+#     while raw_str[ptr] == ' ':
+#         ptr += 1
+#     start_ptr = ptr
+#     if raw_str[ptr] == '<':
+#         ptr += 13
+#
+#         while raw_str[ptr] != ' ':
+#             ptr += 1
